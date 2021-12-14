9127a83f1f84536ff848fdf4172dd1ae890dda48
checker <trinhlehainam@protonmail.com>
2021-12-14 21:41:50 +0900

    Add check ready for player

--- src/states/player.ts
+++ src/states/player.ts
@@ -3,12 +3,14 @@ import { Vector3 } from "./vector3";
 import { Quaternion } from "./quaternion";
 
 export class Player extends Schema {
+    @type("boolean") isReady: boolean;
     @type("number") timeStamp: number;
     @type(Vector3) position: Vector3;
     @type(Quaternion) rotation: Quaternion;
 
     constructor() {
         super()
+        this.isReady = false;
         this.timeStamp = new Date().getTime();
         this.position = new Vector3();
         this.rotation = new Quaternion();
